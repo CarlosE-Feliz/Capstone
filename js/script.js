4df44a614b9d8e25b8d15a6a40f30bc3e811069c
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-08-04 09:36:38 -0600

    Update script.js
    update script.js

--- js/script.js
+++ js/script.js
@@ -61,3 +61,5 @@ const menu = document.querySelector('.open');
 menu.addEventListener('click', () => {document.getElementById('mobile-menu').classList.add('active') })
 const close = document.querySelector('.close');
 close.addEventListener('click', () => {document.getElementById('mobile-menu').classList.remove('active')})
+const ulClose = document.querySelector('.ul-close');
+ulClose.addEventListener('click', () => {document.getElementById('mobile-menu').classList.remove('active')})
