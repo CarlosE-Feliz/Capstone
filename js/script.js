6b5da79673d2688dc0945af5242dc04fcae46cd3
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-08-05 09:19:37 -0600

    Fix menu

--- js/script.js
+++ js/script.js
@@ -47,8 +47,15 @@ createCards();
 
 
 const menu = document.querySelector('.open');
-menu.addEventListener('click', () => {document.getElementById('mobile-menu').classList.add('active') })
+menu.addEventListener('click', () => {
+    document.getElementById('mobile-menu').classList.add('active');
+    document.getElementById('menu_mobile').classList.add('disapear'); 
+})
 const close = document.querySelector('.close');
-close.addEventListener('click', () => {document.getElementById('mobile-menu').classList.remove('active')})
+close.addEventListener('click', () => {
+    document.getElementById('mobile-menu').classList.remove('active');
+    document.getElementById('menu_mobile').classList.remove('disapear'); 
+
+})
 const ulClose = document.querySelector('.ul-close');
 ulClose.addEventListener('click', () => {document.getElementById('mobile-menu').classList.remove('active')})
